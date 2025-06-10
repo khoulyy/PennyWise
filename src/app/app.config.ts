d3c692d4d04f9f609878ce848bb02d9bd4dde579
Abdelrhaman Elkhouly <khoulyyy@gmail.com>
2025-06-10 04:50:19 +0300

    import: environment config in app initialization config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,13 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"pennywise-7b083","appId":"1:1002604848294:web:1b7d33cc53ba3e0368c067","storageBucket":"pennywise-7b083.firebasestorage.app","apiKey":"AIzaSyBdLx1ph28DqbzwZQRWN25n71jTXqQmpS8","authDomain":"pennywise-7b083.firebaseapp.com","messagingSenderId":"1002604848294","measurementId":"G-6CD7GV47WV"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
