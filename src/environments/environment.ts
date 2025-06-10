d2271bc15df85f3c153cde8136dbf942404ac9ef
Abdelrhaman Elkhouly <khoulyyy@gmail.com>
2025-06-10 04:44:12 +0300

    Add: firebase config to environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBdLx1ph28DqbzwZQRWN25n71jTXqQmpS8',
+    authDomain: 'pennywise-7b083.firebaseapp.com',
+    projectId: 'pennywise-7b083',
+    storageBucket: 'pennywise-7b083.firebasestorage.app',
+    messagingSenderId: '1002604848294',
+    appId: '1:1002604848294:web:1b7d33cc53ba3e0368c067',
+    measurementId: 'G-6CD7GV47WV',
+  },
+};
